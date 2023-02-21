b4bcab9b8ae54fe16d53a6da9a696b37848d4e36
Chaos <85056161+allmightychaos@users.noreply.github.com>
2023-02-21 08:53:32 +0100

    Update clearmetadata.rb

--- clearmetadata.rb
+++ clearmetadata.rb
@@ -4,6 +4,7 @@ class Clearmetadata < Formula
     url "https://raw.githubusercontent.com/allmightychaos/clear-where-from/main/rmd.py"
     sha256 "23fd8c892c0b7b01f2789c0e88f9bfec973aeb5bb1e4e4856b4a86edc9d341a6"
     license "MIT"
+    version: "1.0.0"
   
     def install
       bin.install "clear_wherefroms.py"
