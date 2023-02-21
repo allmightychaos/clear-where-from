09f65a72b3e6e69d7dbb3ebff4476679b802b380
Chaos <85056161+allmightychaos@users.noreply.github.com>
2023-02-21 08:55:46 +0100

    Update clearmetadata.rb

--- clearmetadata.rb
+++ clearmetadata.rb
@@ -1,21 +1,20 @@
 class Clearmetadata < Formula
-    desc "A terminal program that clears the 'where from' metadata attribute from a file on macOS"
-    homepage "https://example.com"
-    url "https://raw.githubusercontent.com/allmightychaos/clear-where-from/main/rmd.py"
-    sha256 "23fd8c892c0b7b01f2789c0e88f9bfec973aeb5bb1e4e4856b4a86edc9d341a6"
-    license "MIT"
-    version: "1.0.0"
-  
-    def install
-      bin.install "clear_wherefroms.py"
-      (bin/"clearmetadata").write <<~EOS
-        #!/bin/sh
-        python "#{bin}/clear_wherefroms.py" "$@"
-      EOS
-    end
-  
-    test do
-      system "#{bin}/clearmetadata", "--version"
-    end
+  desc "A terminal program that clears the 'where from' metadata attribute from a file on macOS"
+  homepage "https://example.com"
+  url "https://raw.githubusercontent.com/allmightychaos/clear-where-from/main/rmd.py"
+  sha256 "23fd8c892c0b7b01f2789c0e88f9bfec973aeb5bb1e4e4856b4a86edc9d341a6"
+  license "MIT"
+  version "1.0.0"
+
+  def install
+    bin.install "clear_wherefroms.py"
+    (bin/"clearmetadata").write <<~EOS
+      #!/bin/sh
+      python "#{bin}/clear_wherefroms.py" "$@"
+    EOS
   end
-  
+
+  test do
+    system "#{bin}/clearmetadata", "--version"
+  end
+end
